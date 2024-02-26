6016e72f69fd3e67119b4143e4174ae408059001
SebastianCardenasYanez <160754840+SebastianCardenasYanez@users.noreply.github.com>
2024-02-26 16:07:53 -0500

    conversion grados C a F

---
+++ gradosFahrenheitACelcius.py
@@ -0,0 +1,3 @@
+grdCelsius = float(input("ingrese la temperatura en C que desea conocer en F: "))
+gradFahrenheit = float(grdCelsius * 9/5 + 32 )
+print("el valor en grados Fahrenheit es: ", gradFahrenheit)
