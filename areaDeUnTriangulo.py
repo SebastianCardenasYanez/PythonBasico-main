b60e188c62e0314360d96539f2349ff6cb8c4c3c
SebastianCardenasYanez <160754840+SebastianCardenasYanez@users.noreply.github.com>
2024-02-26 15:40:39 -0500

    Algoritmo area del triangulo

---
+++ areaDeUnTriangulo.py
@@ -0,0 +1,4 @@
+altura = int(input("resgistre la altura del triangulo: "))
+base = int(input("resgistre la base del triangulo: "))
+resultado = (altura * base / 2) 
+print ("el area del triangulo es: ", resultado )
